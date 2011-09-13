25ddfc431133657a8918a9d23efc52a9f2f0cfdb
Scott Sibley <sisibley@gmail.com>
2011-09-13 11:48:05 -0500

    Fix an error.

--- debug.lua
+++ debug.lua
@@ -1,32 +1,12 @@
+local debug = false
+--[===[@debug@
+debug = true
+--@end-debug@]===]
 
-local LibTimer = LibStub("LibScriptableUtilsTimer-1.0")
-local LibCore = LibStub("LibScriptableLCDCoreLite-1.0")
 local LibDriver = LibStub("LibScriptableLCDDriver-1.0")
 
-local environment = {}
-
-
-
---[[
-local function debug()
-do return end
-	local unit = GetMouseoverUnit()
-	print(unit or "None")
-end
-
-LibTimer:New("debug", 1000, false, debug):Start()
-]]
-
-table.insert(Command.Slash.Register("debug"), {function (commandLineParameters)
+table.insert(Command.Slash.Register("lcd4rift"), {function (commandLineParameters)
 	local display = LibDriver:New(environment, environment, "display_startip", _G.LCD4Rift.config, 2)
 	display:Show()
 end, "LibScriptable_1_0", "Slash command"})
 
-LibCore:New(_G, "debug", 3)
-
-local context = UI.CreateContext("HUD")
-bar = UI.CreateFrame("Text", "Text", context)
-bar:SetVisible(true)
-local name = Inspect.Unit.Detail("player").name
-bar:SetText(name)
-bar:SetPoint("CENTER", UIParent, "CENTER")
